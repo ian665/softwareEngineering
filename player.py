b30030e6e34f1d83dcced2b0e4632eb7faeea9b6
D1149754 <auouo12345@gmail.com>
2025-01-07 07:12:23 +0800

    I do something

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     def __init__(self):
         self.x, self.y = 50, 50
         try:
-            self.image = pygame.image.load("./assets/images/player.png")
+            self.image = pygame.image.load("assets\images\player.png")
             self.image = pygame.transform.scale(self.image, (110, 110))
         except pygame.error as e:
             print(f"Error loading player image: {e}")
